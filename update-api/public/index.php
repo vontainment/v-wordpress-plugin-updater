ae503c24c454924dbd2c69f99920d87a6245be5e
Nikolai X. Shadeauxs <services@vontainment.com>
2023-03-12 18:54:34 -0400

    Update index.php

--- update-api/public/index.php
+++ update-api/public/index.php
@@ -7,6 +7,14 @@ Author URI: https://vontainment.com
 */
 
 // Display the content for logged in users
+session_start();
+
+// Check if user is logged in
+if (!isset($_SESSION['logged_in']) || $_SESSION['logged_in'] !== true) {
+    header("Location: login.php");
+    exit;
+}
+
 ?>
 
 <!DOCTYPE html>
