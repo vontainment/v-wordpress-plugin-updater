4c0a4eccf69af2aeda158d2cc71dbe1e2e2b72f1
Nikolai X. Shadeauxs <services@vontainment.com>
2023-03-18 13:54:25 -0400

    up

---
+++ mu-plugin/von-sys-multisite-updater.php
@@ -0,0 +1,111 @@
+<?php
+/*
+Plugin Name: WP Plugin Updater
+Plugin URI: https://vontainment.com
+Description: This plugin updates your WordPress plugins.
+Version: 1.0.0
+Author: Vontainment
+Author URI: https://vontainment.com
+*/
+
+define('VONTMENT_ENDPOINT', 'https://api.vontainment.com/api.php');
+define('VONTMENT_KEY', 'key');    // Define the endpoint URL and key
+
+// Schedule the update check to run every day
+
+function vontmnt_updater_schedule_updates()
+{
+    // Check if it's the main site
+    if (is_main_site()) {
+        if (!wp_next_scheduled('vontmnt_updater_check_updates')) {
+            wp_schedule_event(time(), 'daily', 'vontmnt_updater_check_updates');
+        }
+    }
+}
+
+add_action('vontmnt_updater_check_updates', 'vontmnt_updater_run_updates');
+
+
+function vontmnt_updater_run_updates()
+{
+
+    // Check if it's the main site
+    if (!is_main_site()) {
+        return;
+    }
+
+    // Get the list of installed plugins
+    $plugins = get_plugins();
+
+    // Loop through each installed plugin and check for updates
+    foreach ($plugins as $plugin_path => $plugin) {
+        // Get the plugin slug
+        $plugin_slug = basename($plugin_path, '.php');
+        // Get the installed plugin version
+        $installed_version = $plugin['Version'];
+
+        // Construct the API endpoint URL with the query parameters
+        $api_url = add_query_arg(
+            array(
+                'domain' => urlencode(parse_url(site_url(), PHP_URL_HOST)),
+                'plugin' => urlencode($plugin_slug),
+                'version' => urlencode($installed_version),
+                'key' => VONTMENT_KEY,
+            ),
+            VONTMENT_ENDPOINT
+        );
+
+        // Send the request to the API endpoint
+        $curl = curl_init();
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => $api_url,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_SSL_VERIFYHOST => false,
+            CURLOPT_SSL_VERIFYPEER => false
+        ));
+        $response  = curl_exec($curl);
+        $http_code = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+        curl_close($curl);
+
+        // Get the response body
+        $response_body = $response;
+
+        // Check if the API returned a plugin update
+        if ($http_code == 204) {
+            error_log("$plugin_slug : has no updates");
+        } elseif ($http_code == 401) {
+            error_log("You are not authorized for the Vontainment API");
+        } elseif (!empty($response_body)) {
+            $response_data = json_decode($response_body, true);
+
+            if (isset($response_data['zip_url'])) {
+                $download_url = $response_data['zip_url'];
+
+                // Download the zip file to the upload directory
+                require_once ABSPATH . 'wp-admin/includes/file.php';
+                $upload_dir      = wp_upload_dir();
+                $tmp_file        = download_url($download_url);
+                $plugin_zip_file = $upload_dir['path'] . '/' . basename($download_url);
+
+                // Move the downloaded file to the plugins directory
+                rename($tmp_file, $plugin_zip_file);
+
+                // Unzip the plugin zip file
+                WP_Filesystem();
+                $unzipfile = unzip_file($plugin_zip_file, WP_PLUGIN_DIR);
+
+                // Check if the unzip was successful
+                if (is_wp_error($unzipfile)) {
+                    error_log('Error unzipping plugin file: ' . $unzipfile->get_error_message());
+                } else {
+                    // Delete the plugin zip file
+                    unlink($plugin_zip_file);
+                    error_log("$plugin_slug : Was updated");
+                }
+            } else {
+                error_log("$plugin_slug : Is up-to-date");
+            }
+        }
+    }
+}
--- update-api/log-status.php
+++ update-api/log-status.php
@@ -6,15 +6,24 @@ if (file_exists($log_file)) {
   // read the log file into an array
   $log_array = file($log_file, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
 
-  // group the log entries by domain name
-  $log_by_domain = [];
+  // group the log entries by domain name and date
+  $log_by_domain_date = [];
   foreach ($log_array as $entry) {
     list($domain, $date, $status) = explode(' ', $entry);
-    $log_by_domain[$domain][] = ['date' => $date, 'status' => $status];
+    $log_by_domain_date[$domain][$date] = $status;
   }
 
   // sort the domains alphabetically
-  ksort($log_by_domain);
+  ksort($log_by_domain_date);
+
+  // keep only the latest dated entry for each domain
+  $log_by_domain = [];
+  foreach ($log_by_domain_date as $domain => $entries) {
+    krsort($entries); // sort by date in descending order
+    $latest_date = key($entries);
+    $latest_status = $entries[$latest_date];
+    $log_by_domain[$domain] = ['date' => $latest_date, 'status' => $latest_status];
+  }
 
   // display the log entries in five columns
   $total_domains = count($log_by_domain);
@@ -24,14 +33,13 @@ if (file_exists($log_file)) {
 
   echo '<div class="log-columns">';
 
-  foreach ($log_by_domain as $domain => $entries) {
+  foreach ($log_by_domain as $domain => $entry) {
     // display the domain name
     echo '<div class="log-sub-box">';
     echo '<h3>' . $domain . '</h3>';
 
-    // display the most recent entry for the domain
-    $last_entry = end($entries);
-    echo '<p class="log-entry">' . $last_entry['date'] . ' ' . $last_entry['status'] . '</p>';
+    // display the latest dated entry for the domain
+    echo '<p class="log-entry">' . $entry['date'] . ' ' . $entry['status'] . '</p>';
     echo '</div>';
 
     // if this is the last domain in the column, close the column div and start a new one
@@ -46,6 +54,16 @@ if (file_exists($log_file)) {
   }
 
   echo '</div>';
+
+  // write the updated log array back to the file
+  $log_array_new = [];
+  foreach ($log_by_domain_date as $domain => $entries) {
+    krsort($entries); // sort by date in descending order
+    $latest_date = key($entries);
+    $latest_entry = $domain . ' ' . $latest_date . ' ' . $entries[$latest_date];
+    $log_array_new[] = $latest_entry;
+  }
+  file_put_contents($log_file, implode("\n", $log_array_new));
 } else {
   echo 'Log file not found.';
 }
