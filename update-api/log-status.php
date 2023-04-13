778dadb4c348a5684bdc6723b18a206a914d83dc
Nikolai X. Shadeauxs <services@vontainment.com>
2023-04-12 20:18:14 -0400

    updates
    Better Plugin Sorting,
    Better Log Sorting,
    CSS Fixes

--- update-api/log-status.php
+++ update-api/log-status.php
@@ -25,32 +25,15 @@ if (file_exists($log_file)) {
     $log_by_domain[$domain] = ['date' => $latest_date, 'status' => $latest_status];
   }
 
-  // display the log entries in five columns
-  $total_domains = count($log_by_domain);
-  $domains_per_column = $total_domains > 0 ? ceil($total_domains / 5) : 0;
-  $current_column = 1;
-  $current_domain = 1;
-
-  echo '<div class="log-columns">';
+  // display the log entries in a single row
+  echo '<div class="log-row">';
 
   foreach ($log_by_domain as $domain => $entry) {
-    // display the domain name
+    // display the domain name and the latest dated entry for the domain
     echo '<div class="log-sub-box">';
     echo '<h3>' . $domain . '</h3>';
-
-    // display the latest dated entry for the domain
     echo '<p class="log-entry">' . $entry['date'] . ' ' . $entry['status'] . '</p>';
     echo '</div>';
-
-    // if this is the last domain in the column, close the column div and start a new one
-    if (($current_domain % $domains_per_column == 0) || ($current_column == 5 && $current_domain == $total_domains)) {
-      echo '</div><div class="log-columns">';
-      $current_column++;
-      $domains_left = $total_domains - $current_domain;
-      $domains_per_column = $domains_left > 0 ? ceil($domains_left / (5 - $current_column + 1)) : 0;
-    }
-
-    $current_domain++;
   }
 
   echo '</div>';
